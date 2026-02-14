6392d9c696ad63aae92411c600846ef5643a9b4e
Archana-Archu12 <archuramasamy@gmail.com>
2026-02-14 22:33:39 +0530

    DataType

---
+++ Datatype.js
@@ -0,0 +1,11 @@
+var count=78
+var a= null
+var ad= 1234567887645677n
+var num
+var session=true
+
+console.log("The value is:"+count)
+console.log("The value is:"+count+"type is:"+typeof(count))
+console.log("The value is:"+ad+"type is:"+typeof(ad))
+console.log("The value is:"+num+"type is:"+typeof(num))
+console.log("The value is:"+session+"type is:"+typeof(session))
