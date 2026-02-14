2e06f9bae3bef8fcfe1378b3101a8de15a3d7de0
Archana-Archu12 <archuramasamy@gmail.com>
2026-02-14 22:58:24 +0530

    palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,21 @@
+//using for loop
+
+let a = "malayalam"
+let rev1= ""
+for(let i=a.length-1;i>=0;i--)
+{
+     rev1=rev1+a[i]
+ }
+console.log("The sentence after reversing is :"+rev1)
+if(a==rev1)
+{
+    console.log("Palindrome")
+}else{
+   console.log("not a Palindrome") 
+}
+
+// using reverse keyword
+
 let data='Wow Malayalam Wow'
 let rev=" "
 console.log("The original sentence is :"+data)
@@ -12,5 +30,6 @@ if(orig === final)
 }
 else
 console.log("not a palindrome")
+
 
 
