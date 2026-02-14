5ef2acd437744325251261e758a6729b3568a183
Archana-Archu12 <archuramasamy@gmail.com>
2026-02-14 23:38:49 +0530

    Split

---
+++ splitlength.js
@@ -0,0 +1,11 @@
+let data='Helloo World'
+let split= data.split(" ")
+console.log("the splitted words are: "+split)
+let last= split[split.length-1]
+console.log( "The length of the last word is: "+ last.length)
+
+let msg="fly me to the moon"
+let words= msg.trim().split(" ")
+console.log("the splitted words are: "+ words)
+let word= words[words.length-1]
+console.log( "The length of the last word is: "+ word.length )
