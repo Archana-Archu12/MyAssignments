7a1c39d3fc9a663d1a3fb9bb381e0bcd63efd3df
Archana-Archu12 <archuramasamy@gmail.com>
2026-02-15 23:40:54 +0530

    3.Fibonacci

---
+++ FibonacciSeries.spec.ts
@@ -0,0 +1,13 @@
+function fibonacci(n: number): void {
+    let a = 0;
+    let b = 1;
+
+    for (let i = 0; i < n; i++) {
+        console.log(a);
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+}
+
+fibonacci(11);
